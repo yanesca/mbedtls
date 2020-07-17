060ab349fd7725fe15d9f388b3360badb4d579d1
Janos Follath <janos.follath@arm.com>
2020-07-17 17:42:50 +0100

    Crypto Threat Model: add data flow diagram
    The diagrams are generated using plantuml. Adding a script to make
    re-generating the diagrams more convenient. The generated files are
    added too to enable displaying it on github without involving third
    party services.
    
    Signed-off-by: Janos Follath <janos.follath@arm.com>

---
+++ docs/architecture/threatmodel/gen_diagrams.sh
@@ -0,0 +1,9 @@
+#
+# Generate the diagrams. Don't forget to run this after editing the sources in
+# the 'Uml' directory.
+#
+# Needs to be run from the 'docs/architecture/threat_model/' directory.
+#
+
+plantuml -o ../img -tsvg uml/*.plantuml
+
